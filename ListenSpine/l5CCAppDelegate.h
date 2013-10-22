afd1d8e03b4b261242eaed40a4d0788714ec38df
Hari Karam Singh <harikaram@club15cc.com>
2013-10-22 16:56:25 +0100

    Initial Commit

---
+++ ListenSpine/l5CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  l5CCAppDelegate.h
+//  ListenSpine
+//
+//  Created by Hari Karam Singh on 22/10/2013.
+//  Copyright (c) 2013 Club 15CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface l5CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ListenSpine/l5CCViewController.m
@@ -0,0 +1,29 @@
+//
+//  l5CCViewController.m
+//  ListenSpine
+//
+//  Created by Hari Karam Singh on 22/10/2013.
+//  Copyright (c) 2013 Club 15CC. All rights reserved.
+//
+
+#import "l5CCViewController.h"
+
+@interface l5CCViewController ()
+
+@end
+
+@implementation l5CCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
